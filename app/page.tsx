66f051dd24f9eeef0000591e3493fb8784bb5704
yuyofede <yuyo.vcp@gmail.com>
2026-09-17 13:03:39 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -29,18 +29,13 @@ export default function Home() {
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              servicio
-            </a>{"/servicio "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+              href="/servicio"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              servicio
+            </a>{" "}
+            
             <Image
               className="dark:invert h-[14px] w-4"
               src="/vercel.svg"
