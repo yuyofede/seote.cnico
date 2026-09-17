e85dad76fa5df44bab40452e4dac2fb085178c5c
yuyofede <yuyo.vcp@gmail.com>
2026-09-17 12:36:30 +0200

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Este es el encabezado de la pagina
+            Pagina de contacto
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Prueba de etiqueta {" "}
--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
             </a>{" "}
             or the{" "}
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://seote-cnico.vercel.app/contact"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Learning
