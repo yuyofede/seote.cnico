a4782090565dcfb8a7074caf259ad627309a8642
yuyofede <yuyo.vcp@gmail.com>
2026-09-17 13:08:13 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -36,23 +36,7 @@ export default function Home() {
               servicio
             </a>{" "}
             
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            
         </div>
       </main>
     </div>
